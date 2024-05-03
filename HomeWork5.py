1135589c3b12086baa2eb045b9853f3b7ee81b47
GluttonyGula <168459225+GluttonyGula@users.noreply.github.com>
2024-05-03 22:48:24 +0400

    HomeWork5.py

--- HomeWork5.py
+++ HomeWork5.py
@@ -1,7 +1,7 @@
 my_list = ['apple', 'peach', 'banana', 'melon', 'grape']
 print(my_list)
 print(my_list[0], my_list[-1])
-print(my_list[0:5:4]) #невозможно использовать без знания кол-во элементов в списке, вывод списком. Занимает больше места или времени?
+print(my_list[0:5:4])
 print(my_list[2:5])
 my_list[2] = 'kiwi'
 print(my_list)
